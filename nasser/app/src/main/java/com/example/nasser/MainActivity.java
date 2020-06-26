df72a24797f1c11ee9fd59d04a18fc87237b7832
Nasser Khaled <nasserhuss123@gmail.com>
2020-06-26 21:24:05 +0300

    HW4 Done

--- nasser/app/src/main/java/com/example/nasser/MainActivity.java
+++ nasser/app/src/main/java/com/example/nasser/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.telephony.mbms.MbmsErrors;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.telephony.mbms.MbmsErrors.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,37 +27,45 @@ public class MainActivity extends AppCompatActivity {
       final EditText mid = findViewById(R.id.editText3);
       final EditText Final = findViewById(R.id.editText4);
       final Button c = findViewById(R.id.button);
-      final Button Re = findViewById(R.id.button2);
+      final Button Reset = findViewById(R.id.button2);
       final TextView f = findViewById(R.id.textView2);
+
+      Reset.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              quz.setText("");
+              Hw.setText("");
+              mid.setText("");
+              Final.setText("");
+              f.setText("");
+          }
+      });
+
+
+
       c.setOnClickListener(new View.OnClickListener() {
           @Override
-          public void onClick(View view){
-              double  n1 = Double.parseDouble(quz.getText().toString());
-              double  n2 = Double.parseDouble(Hw.getText().toString());
-           double  n3 = Double.parseDouble(mid.getText().toString());
-           double  n4 = Double.parseDouble(Final.getText().toString());
+          public void onClick(View view) {
+              double n1 = Double.parseDouble(quz.getText().toString());
+              double n2 = Double.parseDouble(Hw.getText().toString());
+              double n3 = Double.parseDouble(mid.getText().toString());
+              double n4 = Double.parseDouble(Final.getText().toString());
 
-           double result = n1 + n2 + n3 + n4 ;
-           Toast.makeText(MainActivity.this, "the addition is : "+result,Toast.LENGTH_LONG).show();
-
-
+              double result = n1 + n2 + n3 + n4;
+              Toast.makeText(MainActivity.this, "the addition is : " + result, Toast.LENGTH_LONG).show();
 
 
-            if (result>=90){f.setText('A'); }
-            else if (result>=80){f.setText('B'); }
-            else if (result>=70){f.setText('C'); }
-            else if (result>=60){f.setText('D'); }
-            else { f.setText('F');}
-
-
-
-
-
-
-
-
-
-
+              if (result >= 90) {
+                  f.setText("your grade is A");
+              } else if (result >= 80) {
+                  f.setText("your grade is B");
+              } else if (result >= 70) {
+                  f.setText("your grade is C");
+              } else if (result >= 60) {
+                  f.setText("your grade is D");
+              } else {
+                  f.setText("your grade is F");
+              }
 
 
 
@@ -63,4 +75,5 @@ public class MainActivity extends AppCompatActivity {
 
 
   }
+
 }
